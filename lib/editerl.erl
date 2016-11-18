df85124a3c66d55eaec91b118aba2ff48ec9f386
Immortalin <immortalin@icloud.com>
2016-11-18 17:35:58 +0800

    Looping is working;  i.e. window is not closing immediately

--- lib/editerl.erl
+++
@@ -1,9 +0,0 @@
--module(editerl).
--export([start/2]).
-
-start(_Type, _Args) ->
-    {ok,self()}.
-
-Wx = wx:new().
-Frame = wxFrame:new(Wx, 43, "Hello Erlang!").
-wxFrame:show(Frame).
--- src/editerl.erl
+++ src/editerl.erl
@@ -4,10 +4,21 @@
 
 start(_Type, _Args) ->
     show_gui(),
+    loop(),
     {ok, self()}.
 
 show_gui()->
     Wx = wx:new(),
     Frame = wxFrame:new(Wx, 43, "Hello Erlang!"),
     wxFrame:show(Frame),
-    show_gui().
+    show_gui(3,1).
+
+show_gui(N, Acc) when Acc < N ->
+    io:fwrite("stuff\n"),
+    show_gui(N, Acc + 1);
+show_gui(N, Acc) when Acc =:= N ->
+    io:format("Last stuff! ~n"),
+    ok.
+
+loop()->
+    loop().
